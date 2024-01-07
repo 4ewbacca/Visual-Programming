acf901873376abb62d2af214d118a1b097b4f42a
4ewbacca <95045595+4ewbacca@users.noreply.github.com>
2024-01-07 20:40:01 +0200

    Add files via upload

---
+++ Assignment_6/Assignment_6/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace Assignment_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.RockButton = new System.Windows.Forms.Button();
+            this.PaperButton = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(382, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Computer Choice:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(386, 116);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // RockButton
+            // 
+            this.RockButton.Location = new System.Drawing.Point(74, 115);
+            this.RockButton.Name = "RockButton";
+            this.RockButton.Size = new System.Drawing.Size(75, 23);
+            this.RockButton.TabIndex = 2;
+            this.RockButton.Text = "Rock";
+            this.RockButton.UseVisualStyleBackColor = true;
+            this.RockButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PaperButton
+            // 
+            this.PaperButton.Location = new System.Drawing.Point(74, 157);
+            this.PaperButton.Name = "PaperButton";
+            this.PaperButton.Size = new System.Drawing.Size(75, 23);
+            this.PaperButton.TabIndex = 3;
+            this.PaperButton.Text = "Paper";
+            this.PaperButton.UseVisualStyleBackColor = true;
+            this.PaperButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(74, 203);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Scissors";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(78, 389);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Your Choice:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(386, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.PaperButton);
+            this.Controls.Add(this.RockButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button RockButton;
+        private System.Windows.Forms.Button PaperButton;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Assignment_6/Assignment_6/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_6
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+            
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+            
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PlayGame("rock");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PlayGame("paper");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            PlayGame("scissors");
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void PlayGame(string userChoice)
+        {
+            int computerChoice = random.Next(1, 4); 
+
+            DisplayComputerChoice(computerChoice); 
+
+            string result = DetermineWinner(userChoice, computerChoice);
+
+            label3.Text = result; 
+        }
+        private void DisplayComputerChoice(int choice)
+        {
+            string computerChoice = "";
+            switch (choice)
+            {
+                case 1:
+                    computerChoice = "rock";
+                    break;
+                case 2:
+                    computerChoice = "paper";
+                    break;
+                case 3:
+                    computerChoice = "scissors";
+                    break;
+            }
+            textBox1.Text = computerChoice;
+        }
+
+        private string DetermineWinner(string userChoice, int computerChoice)
+        {
+            string result = "";
+            switch (userChoice)
+            {
+                case "rock":
+                    result = computerChoice == 1 ? "Try Again!" : (computerChoice == 2 ? "You Lost!" : "Congratulations You Won!");
+                    break;
+                case "paper":
+                    result = computerChoice == 2 ? "Try Again!" : (computerChoice == 3 ? "You Lost!" : "Congratulations You Won!");
+                    break;
+                case "scissors":
+                    result = computerChoice == 3 ? "Try Again!" : (computerChoice == 1 ? "You Lost!" : "Congratulations You Won!");
+                    break;
+                default:
+                    result = "Please select an option.";
+                    break;
+            }
+            return result;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
